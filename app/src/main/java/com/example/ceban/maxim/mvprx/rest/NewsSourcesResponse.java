121ae1a8fce8e78e22b40cc4f99216095ba2658e
mceban <maximcheban991@gmail.com>
2016-11-25 00:42:10 +0200

    'fff'

--- app/src/main/java/com/example/ceban/maxim/mvprx/rest/NewsSourcesResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.ceban.maxim.mvprx.rest;
-
-import com.example.ceban.maxim.mvprx.entities.Source;
-
-public class NewsSourcesResponse extends Response<Source> {
-}
--- app/src/main/java/com/example/ceban/maxim/mvprx/ui/view/holder/ViewHolder.java
+++
@@ -1,4 +0,0 @@
-package com.example.ceban.maxim.mvprx.ui.view.holder;
-public interface ViewHolder<T>{
-
-}
